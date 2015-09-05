bc5accd23b4984e13ffe1b435aefe6f88a627053
anton.voskobovich <anton.voskobovich@gmail.com>
2015-09-05 14:21:17 +0300

    transaction view added

--- js/main.js
+++ js/main.js
@@ -8,7 +8,20 @@ window.app = app;
 
 //$('.cal1').clndr();
 
-var source = $('#transactions-template').html();
-var template = Handlebars.compile(source);
-var result = template({transactions: transactions});
-$('.transactions').append(result);
+
+var TransactionsView = Backbone.View.extend({
+
+  className: 'transactions',
+
+  render: function() {
+      var source = $('#transactions-template').html();
+      var template = Handlebars.compile(source);
+      var result = template({transactions: transactions});
+      $('.transactions').html(result);
+      return this;
+  }
+
+});
+
+var transactionsView = new TransactionsView();
+transactionsView.render();
