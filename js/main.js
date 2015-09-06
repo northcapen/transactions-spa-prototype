5a8861ff7aec3c08502eb25f2dffc38355d3e8e4
anton.voskobovich <anton.voskobovich@gmail.com>
2015-09-06 10:12:50 +0300

    two basic views

--- js/main.js
+++ js/main.js
@@ -3,22 +3,43 @@ var app = {
 
 window.app = app;
 
-//$('.cal1').clndr();
 
 var Backbone = require('backbone');
 
-var TransactionsView = Backbone.View.extend({
+var CalendarView = Backbone.View.extend({
+    className: 'cal1',
 
-  className: 'transactions',
+    initialize: function() {
+       this.render();
+    },
 
-  render: function() {
-      var source = $('#transactions-template').html();
-      var template = Handlebars.compile(source);
-      var result = template({transactions: transactions});
-      $('.transactions').html(result);
-      return this;
-  }
+    render: function() {
+        $('.cal1').clndr({
+            clickEvents: {
+                click: function (target) {
+                    console.log('click');
+                }
+            }
+        });
+    }
 });
 
-var transactionsView = new TransactionsView();
-transactionsView.render();
+
+var TransactionsView = Backbone.View.extend({
+    className: 'transactions',
+
+    initialize: function() {
+      this.render();
+    },
+
+    render: function () {
+        var source = $('#transactions-template').html();
+        var template = Handlebars.compile(source);
+        var result = template({transactions: transactions});
+        $('.transactions').html(result);
+        return this;
+    }
+});
+
+new CalendarView();
+new TransactionsView();
