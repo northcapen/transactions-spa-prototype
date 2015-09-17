fd552652b78e3cd6dba1cfbc38419c6a2676ab43
anton.voskobovich <anton.voskobovich@gmail.com>
2015-09-17 10:16:42 +0300

    filter panel view listens calendar click

--- js/main-amp.js
+++ js/main-amp.js
@@ -1,12 +1,14 @@
 var AmpersandView = require('ampersand-view');
 var AmpersandModel = require('ampersand-model');
 _ = require('underscore');
+var Handlebars = require('handlebars');
+
 
 
 var FilterModel = AmpersandModel.extend({
     props: {
-        startDate: ['object', true, function() {return moment(); } ],
-        endDate: ['object', true, function() { moment().add(1, 'd'); }]
+        startDate: ['object', true, function() {return moment();      } ],
+        endDate  : ['object', true, function() { var m = moment(); m.add(1, 'd'); return m; }]
     },
 
     toJSON: function() {
@@ -50,5 +52,41 @@ var CalendarView = AmpersandView.extend({
     }
 });
 
+
+var FilterPanelView = AmpersandView.extend({
+    template: Handlebars.compile($('#filter-panel-template').html()),
+
+    initialize: function (options) {
+        this.filter = options.filter;
+        this.listenTo(this.filter, 'change', this.render);
+        this.render();
+    },
+
+    events: {
+        'change' : 'change',
+        'submit form' : 'submit'
+    },
+
+    change: function (e) {
+        this.filter.set({
+            'startDate': moment($('input[name=startDate]', this.$el).val()),
+            'endDate': moment($('input[name=endDate]', this.$el).val()),
+            'details': $('input[name=details]', this.$el).val()
+        });
+        return false;
+    },
+
+    render: function () {
+        this.renderWithTemplate(this.filter.toJSON());
+        return this;
+    },
+
+    submit : function(e) {
+        e.preventDefault();
+    }
+});
+
 var filterModel = new FilterModel();
 new CalendarView({filter: filterModel});
+new FilterPanelView({filter: filterModel, el: $('.filter-panel').get(0)});
+
