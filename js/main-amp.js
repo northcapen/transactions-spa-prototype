375a52d92b3a14a4503c7b47ec8f9a5a5d6d3795
anton.voskobovich <anton.voskobovich@gmail.com>
2015-09-18 10:23:04 +0300

    transactions introduced

--- js/main-amp.js
+++ js/main-amp.js
@@ -1,9 +1,11 @@
 var AmpersandView = require('ampersand-view');
 var AmpersandModel = require('ampersand-model');
+var AmpersandRestCollection = require('ampersand-rest-collection');
 _ = require('underscore');
 var Handlebars = require('handlebars');
 
-
+var moment = require('moment');
+var momentRange = require('moment-range');
 
 var FilterModel = AmpersandModel.extend({
     props: {
@@ -23,6 +25,48 @@ var FilterModel = AmpersandModel.extend({
         return moment.range(this.get('startDate').clone(), this.get('endDate').clone());
     }
 });
+
+
+var TransactionsCollection = AmpersandRestCollection.extend({
+        url: 'http://localhost:3000/transactions',
+
+        initialize: function(models, options) {
+            if(!options) return;
+            this.txFilter = options.filter;
+            this.listenTo(options.filter, 'change', this.processFilterChange);
+        },
+
+        processFilterChange: function() {
+            if(this.loadedFilterRange && this.loadedFilterRange.contains(this.txFilter.toRange().start) && this.loadedFilterRange.contains(this.txFilter.toRange().end)) {
+                this.trigger('update');
+                return;
+            }
+
+            var collection = this;
+            this.fetch({data: this.txFilter.toJSON(), success: function() {
+                collection.loadedFilterRange = collection.txFilter.toRange();
+                collection.trigger('update');
+                collection.trigger('reset');
+                console.log('New ', collection.length,  ' transactions loaded ', ' for range ', collection.loadedFilterRange);
+            }});
+        },
+
+        filtered: function() {
+            return new TransactionsCollection(this.filterTransactions(this.txFilter));
+        },
+
+        filterTransactions: function(txFilter) {
+            var text = txFilter.details;
+            return this.filter(function (tx) {
+                return moment(tx.time).isBetween(txFilter.startDate, txFilter.endDate) && (!text || (tx.get('description') + ' ' + tx.get('partyName')).toLowerCase().indexOf(text.toLowerCase()) > 0);
+            });
+        },
+
+        filterTransactionsPerDay : function (day) {
+            return this.filterTransactions(new FilterModel({startDate: day, endDate: day.clone().add(1, 'd')}));
+        }
+    }
+);
 
 
 var CalendarView = AmpersandView.extend({
@@ -107,8 +151,24 @@ var TransactionsHeader = AmpersandView.extend({
     }
 });
 
+var TransactionsView = AmpersandView.extend({
+    template: Handlebars.compile('<div class="transactions">' + $('#transactions-template').html() + '</div>'),
+
+    //collection name?
+    initialize: function (options) {
+        this.listenTo(this.collection, 'update', this.render);
+    },
+
+    render: function () {
+        this.renderWithTemplate({transactions: this.collection.filtered().toJSON()});
+        return this;
+    }
+});
+
 var filterModel = new FilterModel();
+var transactions = new TransactionsCollection([], {filter: filterModel});
+
 new CalendarView({filter: filterModel});
 new FilterPanelView({filter: filterModel, el: $('.filter-panel').get(0)});
 new TransactionsHeader({model: filterModel, el: $('.header').get(0)});
-
+new TransactionsView({collection: transactions, el: $('.transactions').get(0)});
